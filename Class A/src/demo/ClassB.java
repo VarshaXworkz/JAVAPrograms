323704697a0420dcba7665cf0d7352fc653d2335
VarshaXworkz <varshusannu18@gmail.com>
2025-08-22 14:11:22 +0530

    Add files via upload

---
+++ Class A/src/demo/ClassB.java
@@ -0,0 +1,12 @@
+package demo;
+
+public class ClassB {
+	public static void main(String[] args) {
+		ClassC obj=new ClassC(2,2);
+	     int sum=obj.x+obj.y;
+		//System.out.println(obj);
+		System.out.println("The Sum is"+sum);
+		
+	}
+
+}
---
+++ Class A/src/demo/ClassC.java
@@ -0,0 +1,11 @@
+package demo;
+
+public class ClassC {
+	int x,y;
+	ClassC(int x,int y){
+		this.x=x;
+		this.y=y;
+	}
+	
+
+}
